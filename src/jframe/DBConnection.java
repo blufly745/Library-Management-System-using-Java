ae0221430a0533607bac1748c5866d93653f52e2
blufly745 <3379444504@qq.com>
2026-04-24 21:52:29 +0800

    Update DBConnection.java

--- src/jframe/DBConnection.java
+++ src/jframe/DBConnection.java
@@ -11,13 +11,7 @@ public class DBConnection {
     private static final String PASSWORD = "123456";
     //获取连接方法
     public static Connection getConnection() throws SQLException {
-        try {
-        	//加载mysql驱动，可省
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();  //打印异常对象完整调用堆栈轨迹，哪一行出错，经过哪些方法调用
-        }
-        //其实前面都可省，只保留这一句
+        
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
